2ff2e488a7fd42000201c3e7de73ae4f766edf84
vivianelopes <vivianelopesalmeida@yahoo.com.br>
2024-01-02 15:53:21 -0300

    Alterando atributos privados

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,10 +3,10 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        private string Modelo { get; }
+        private string Modelo;
 
-        private string IMEI { get;}
-        private int Memoria { get; }
+        private string IMEI;
+        private int Memoria;
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
