27da4cfd515abf745ed2603e1a793a05d5945567
vivianelopes <vivianelopesalmeida@yahoo.com.br>
2024-01-02 15:26:52 -0300

    Construcao das Smartphone e Nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia cel = new Nokia("598614", "Modelo 7", "1111999", 67);
+
+cel.Ligar();
+cel.ReceberLigacao();
+cel.InstalarAplicativo("Banco Itau");
+
